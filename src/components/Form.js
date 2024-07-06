caa231411c95586fe22149aaa1cdcb0112a087e5
Yevheniia Kovzikova <144259600+Mahaonn@users.noreply.github.com>
2024-07-06 22:27:03 +0200

    Added data transfer from the user to the App component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,30 @@
+// Form.js
 import React, { useState } from "react";
 
 import "../styles/Form.css";
 
 const Form = ({ onCityChange }) => {
   const [userInputCity, setUserInputCity] = useState("");
+
   const handleCityChange = (e) => {
     setUserInputCity(e.target.value);
+    onCityChange(e.target.value); // Викликаємо функцію для передачі міста
   };
   const handleSearch = () => {
-    onCityChange(userInputCity);
+    onCityChange(userInputCity); // Викликаємо функцію для передачі міста
   };
   return (
     <div className="form">
-      <form className="search__form">
-        <input
-          className="search__input"
-          type="text"
-          placeholder="Enter a city.."
-          autoFocus
-          required
-          value={userInputCity}
-          onChange={handleCityChange}
-        />
-        <button type="button" className="search__btn" onClick={handleSearch}>
-          Search
-        </button>
-      </form>
+      <input
+        className="search__input"
+        type="text"
+        placeholder="Enter a city.."
+        value={userInputCity}
+        onChange={handleCityChange}
+      />
+      <button type="button" className="search__btn" onClick={handleSearch}>
+        Search
+      </button>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
+// Header.js
+import React from "react";
 import Form from "./Form";
+
 import "../styles/Header.css";
 
-const Header = () => {
-  const handleCityChange = (city) => {
-    console.log("Місто в компоненті Header:", city);
-  };
+export default function Header({ onCityChange }) {
   return (
     <header className="header">
       <h1 className="title">Weather App</h1>
-      <Form onCityChange={handleCityChange} />
+      <Form onCityChange={onCityChange} />{" "}
+      {/* Передаємо функцію для обробки міста */}
     </header>
   );
-};
-export default Header;
+}
