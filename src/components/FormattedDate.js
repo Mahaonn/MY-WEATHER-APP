2b2b6358678c8878531d0d043a988e743988be2c
Yevheniia Kovzikova <144259600+Mahaonn@users.noreply.github.com>
2024-07-21 18:01:59 +0200

    Fixed a bug with minutes and hours that were not updated.

--- src/components/FormattedDate.js
+++ src/components/FormattedDate.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 const FormattedDate = (props) => {
-  const [caurrentTime, setCurrentTime] = useState(new Date());
+  const [currentTime, setCurrentTime] = useState(new Date());
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentTime(new Date());
@@ -26,9 +26,9 @@ const FormattedDate = (props) => {
   ];
   let day = days[props.date.getDay()];
   let month = months[props.date.getMonth()];
-  const hours = props.date.getHours().toString().padStart(2, "0");
-  const minutes = props.date.getMinutes().toString().padStart(2, "0");
-  const seconds = caurrentTime.getSeconds().toString().padStart(2, "0");
+  const hours = currentTime.getHours().toString().padStart(2, "0");
+  const minutes = currentTime.getMinutes().toString().padStart(2, "0");
+  const seconds = currentTime.getSeconds().toString().padStart(2, "0");
 
   return (
     <div>
